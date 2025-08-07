53f3a5d088169765b6ecb2d3b20e4df3eb83d512
Poonam <poonam.sonawane68@gmail.com>
2025-08-07 21:57:17 +0530

    Correct footer color.

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export function Footer() {
   return (
-    <footer className="bg-gray-900 text-white">
+    <footer className="bg-foreground text-white">
       {/* Main Footer */}
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
         <div className="flex flex-col lg:flex-row lg:justify-between gap-8">
